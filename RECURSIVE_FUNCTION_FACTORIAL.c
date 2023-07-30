7bc32b5393db1ee022ac913025e02809209f2589
RAHULCYBERGEEK <140412922+RAHULCYBERGEEK@users.noreply.github.com>
2023-07-30 07:37:38 -0700

    Add files via upload

---
+++ RECURSIVE_FUNCTION_FACTORIAL.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int factorial(int number)
+{
+   if (number==1 || number==0)
+   {
+      return 1;
+   }
+
+   else
+        return(number*factorial(number-1));
+}
+
+int main()
+{
+   int num;
+   printf("enter the number you want the factorial of\n");
+   scanf("%d",&num);
+   printf("THE FACTORIAL OF %d IS %d", num, factorial(num));
+
+
+   return 0;
+}
