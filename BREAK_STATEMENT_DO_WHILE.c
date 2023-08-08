c437f546714e7736744a230485e1582359d792ac
RAHULCYBERGEEK <140412922+RAHULCYBERGEEK@users.noreply.github.com>
2023-08-08 00:27:41 -0700

    Add files via upload

---
+++ BREAK_STATEMENT_DO_WHILE.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   do
+   {
+
+      printf("ENTER NUMBER : ");
+      scanf("%d ", &n);
+      printf("%d\n",n);
+
+      if(n%2!=0)
+      {
+         break;
+      }
+
+
+
+   }
+
+   while(1);
+   printf("THANK YOU : ");
+   return 0;
+}
